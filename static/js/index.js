9ebabd4d59700f1c45293b79135c066cf1cfc64a
somnoynadno <somnoynadno@yandex.ru>
2020-02-18 20:00:18 +0700

    Functionality added

--- static/js/index.js
+++ static/js/index.js
@@ -3,14 +3,8 @@
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(main);
 
-function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-        ['Day', 'Price'],
-        ['2004',  1000],
-        ['2005',  1170],
-        ['2006',  660],
-        ['2007',  1030],
-    ]);
+function drawChart(dots) {
+    var data = google.visualization.arrayToDataTable(dots);
 
     var options = {
         title: 'Exchange rate',
@@ -24,16 +18,23 @@ function drawChart() {
 }
 
 class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.sell = this.props.sell.bind(this);
+        this.buy = this.props.buy.bind(this);
+    }
+
     render() {
         return (
-            <div className="row">
+            <div className="row mt-2">
                 <div className="col">
                     <form>
                         <div className="form-group">
                             <label htmlFor="sell-amount">How much you wanna sell?</label>
                             <input type="text" className="form-control" id="sell-amount" />
                         </div>
-                        <div className="btn btn-primary">
+                        <div className="btn btn-primary" onClick={this.sell}>
                             Sell
                         </div>
                     </form>
@@ -42,9 +43,9 @@ class Menu extends React.Component {
                     <form>
                         <div className="form-group">
                             <label htmlFor="buy-amount">How much you wanna buy?</label>
-                            <input type="text" className="form-control" id="buy-amount"/>
+                            <input type="text" className="form-control" id="buy-amount" />
                         </div>
-                        <div className="btn btn-primary">
+                        <div className="btn btn-primary" onClick={this.buy}>
                             Buy
                         </div>
                     </form>
@@ -74,19 +75,19 @@ class Graphics extends React.Component {
     }
 }
 
-class Statistics extends React.Component {
+class Statistics extends React.Component {  
     render() {
         return (
             <div className="row mt-4">
                 <div className="col">
                     <h2>You have:</h2>
-                    <p>Money: 100</p>
-                    <p>Securities: 0</p>
+                    <p>Money: {this.props.money} </p>
+                    <p>Securities: {this.props.securities} </p>
                 </div>
                 <div className="col">
                     <h2>Current:</h2>
-                    <p>Day: 1</p>
-                    <p>Price: 100</p>
+                    <p>Day: {this.props.day} </p>
+                    <p>Price: {this.props.price} </p>
                 </div>
             </div>
         )
@@ -94,13 +95,76 @@ class Statistics extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.k = 0.5;
+        this.dots = [['Day', 'Price']];
+
+        this.state = {
+            currentDay: -1, 
+            moneyAmount: 300,
+            securitiesAmount: 3,
+            securityCurrentPrice: 50
+        };
+
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+
+    tick() {
+        let newPrice = this.state.securityCurrentPrice * (1 + this.k * (Math.random() - 0.5));
+
+        this.setState({
+            currentDay: this.state.currentDay + 1,
+            securityCurrentPrice: newPrice
+        });
+
+        this.dots.push([this.state.currentDay + 1, newPrice]);
+
+        drawChart(this.dots);
+        console.log(this.state);
+    }
+
+    sell() {
+        let amount = Number($('#sell-amount').val());
+
+        if (!isNaN(amount) && amount > 0 && this.state.securitiesAmount - amount >= 0) {
+            this.setState({
+                moneyAmount: this.state.moneyAmount + this.state.securityCurrentPrice*amount,
+                securitiesAmount: this.state.securitiesAmount - amount
+            });
+        }
+    }
+
+    buy() {
+        let amount = Number($('#buy-amount').val());
+
+        if (!isNaN(amount) && amount > 0 && this.state.moneyAmount >= this.state.securityCurrentPrice*amount) {
+            this.setState({
+                moneyAmount: this.state.moneyAmount - this.state.securityCurrentPrice*amount,
+                securitiesAmount: this.state.securitiesAmount + amount
+            });
+        }
+    }
+
     render() {
         return (
             <div>
                 <h1>Economy</h1>
-                <Menu />
-                <Statistics />
-                <Graphics />
+                <Menu 
+                    // callbacks
+                    sell={this.sell.bind(this)} 
+                    buy={this.buy.bind(this)} 
+                />
+                <Statistics 
+                    day={this.state.currentDay} 
+                    money={this.state.moneyAmount}
+                    securities={this.state.securitiesAmount}
+                    price={this.state.securityCurrentPrice}
+                />
+                <Graphics 
+                    dots={this.state.dots}
+                />
             </div>
         )
     }
@@ -111,5 +175,4 @@ function main() {
         <Game />,
         document.getElementById('container')
     );
-    drawChart();
 }
